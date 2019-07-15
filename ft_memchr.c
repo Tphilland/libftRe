6c569d290d735a4e2e399b8f8f72ee246bbd2f48
Thato Phillander <tphillan@c3r7s2.wethinkcode.co.za>
2019-07-15 14:37:53 +0200

    The memchr() function locates the first occurrence of c (converted to an unsigned char) in string s.

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,8 +6,29 @@
 /*   By: tphillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 20:22:57 by tphillan          #+#    #+#             */
-/*   Updated: 2019/07/14 20:23:33 by tphillan         ###   ########.fr       */
+/*   Updated: 2019/07/15 14:35:54 by tphillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	const char	*usrc;
+	unsigned char	uc;
+
+	usrc = (const char *)s;
+	uc = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		if (usrc[i] == uc)
+		{
+			return (s);
+		}
+		i++;
+		usrc++;
+	}
+	return (NULL);
+}
