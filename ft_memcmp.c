cf9ed8364116d420b039e50e71999f0f5273cd77
Thato Phillander <tphillan@c3r7s2.wethinkcode.co.za>
2019-07-15 14:50:53 +0200

    The memcmp() function compares byte string s1 against byte string s2. Both strings are assumed to be n bytes long.

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,8 +6,27 @@
 /*   By: tphillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 20:24:05 by tphillan          #+#    #+#             */
-/*   Updated: 2019/07/14 20:24:55 by tphillan         ###   ########.fr       */
+/*   Updated: 2019/07/15 14:31:42 by tphillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+
+	const char	*str1;
+	const char	*str2;
+
+	str1 = (const char *)s1;
+	str2 = (const char *)s2;
+	if (n == 0)
+		return (0);
+	while ((n > 1) && (*str1 == *str2))
+	{
+		str1++;
+		str2++;
+		n--;
+	}
+	return (*str1 - *str2);
+}
