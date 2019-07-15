d012f7a67bc26941036fd76876f5f07d8a5dacdd
Thato Phillander <tphillan@c3r7s2.wethinkcode.co.za>
2019-07-15 14:52:48 +0200

    The memmove() function copies len bytes from string src to string dst. The two strings may overlap; the copy is always done in a non-destructive manner.

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,8 +6,37 @@
 /*   By: tphillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 20:20:48 by tphillan          #+#    #+#             */
-/*   Updated: 2019/07/14 20:21:10 by tphillan         ###   ########.fr       */
+/*   Updated: 2019/07/15 14:44:00 by tphillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+	char	*udst;
+	const char	*usrc;
+
+	i = 0;
+	udst = (char *)dst;
+	usrc = (const char *)src;
+	if (!dst && !src)
+		return (NULL);
+	if (usrc < udst)
+	{
+		while (len-- > 0)
+		{
+			udst[len] = usrc[len];
+		}
+	}
+	else
+	{
+		while (i < len)
+		{
+			udst[i] = usrc[i];
+			i++;
+		}
+	}
+	return (dst);
+}
