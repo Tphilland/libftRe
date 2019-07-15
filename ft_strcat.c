e9fd154676587364b9a73ae2796c289dc27f64fa
Thato Phillander <tphillan@c3r7s2.wethinkcode.co.za>
2019-07-15 14:54:56 +0200

    The strcat() and strncat() functions append a copy of the null-terminated string s2 to the end of the null-terminated string s1, then add a terminating '\0'.

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,8 +6,26 @@
 /*   By: tphillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 20:27:38 by tphillan          #+#    #+#             */
-/*   Updated: 2019/07/14 20:27:47 by tphillan         ###   ########.fr       */
+/*   Updated: 2019/07/15 14:46:21 by tphillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strcat(char *restrict s1, const char *restrict s2)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (s1[i])
+		i++;
+	while (s2[j])
+	{
+		s1[i + j] = s2[j];
+		j += 1;
+	}
+	s1[i + j] = '\0';
+	return (s1);
+}
