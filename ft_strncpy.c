18dc547e652256f044a6a3c2287283a2b7145a9c
Thato Phillander <tphillan@c3r7s2.wethinkcode.co.za>
2019-07-15 15:01:56 +0200

    function copy the string src to dst including the terminating NULL character

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,8 +6,25 @@
 /*   By: tphillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 20:26:27 by tphillan          #+#    #+#             */
-/*   Updated: 2019/07/14 20:27:28 by tphillan         ###   ########.fr       */
+/*   Updated: 2019/07/15 14:49:12 by tphillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strncpy(char *dst, const char *src, size_t len)
+{
+	int i;
+	size_t j;
+
+	i = 0;
+	j = (size_t)i;
+	while (src[i] != '\0' && j <= len)
+	{
+		dst[i] = src[i];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
