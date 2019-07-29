fa5237f36dd5e92fbec816009549b17a9c9196a9
Tphilland <tphillan@student.wethinkcode.co.za>
2019-07-29 15:17:30 +0200

    e batla ke e testile, maar ke sure ngwana ke ene

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,14 +3,14 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t i;
-	int res;
 
 	i = 0;
-	res = 0;
-	while (i < n)
-	{
-		res = ft_strcmp(*s1, *s2);
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < n)
 		i++;
-	}
-	return (res);
+	if (s1[i] == s2[i])
+		return (0);
+	else if (s1[i] > s2[i])
+		return (1);
+	else
+		return (-1);
 }
