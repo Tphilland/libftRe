f27727859d222eec826c555df25b713375aec7b2
Tphilland <tphillan@student.wethinkcode.co.za>
2019-07-29 11:38:02 +0200

    atoi converts a string into an integer

---
+++ ft_atoi.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int i;
+	int sign;
+	int result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + ( (int)str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
