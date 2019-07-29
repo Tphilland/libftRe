4e77b287978c2bf900457bb16ac24e1d302c2eb7
Tphilland <tphillan@student.wethinkcode.co.za>
2019-07-29 13:43:42 +0200

    am.

---
+++ ft_strcmp.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	if (s1[i] == s2[i])
+		return (0);
+	else if (s1[i] > s2[i])
+		return (1);
+	else
+		return (-1);
+}
