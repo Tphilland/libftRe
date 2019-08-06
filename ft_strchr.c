1f561f4f126756bb8a429f571389d433c98b66e0
Tphilland <tphillan@student.wethinkcode.co.za>
2019-08-06 14:59:02 +0200

    function locates the ﬁrst occurrence of c (converted to a char) in the string pointed to by s. The terminating null character is considered to be part of the string

--- ft_strchr.c
+++ ft_strchr.c
@@ -2,8 +2,11 @@
 
 char *ft_strchr(const char *s, int c)
 {
-	while(!*s != (char)c)
-		if(!*s++)
-			return(0);
+	while (*s != (char)c)
+	{
+		if (!*s)
+			return(NULL);
+		*s++;
+	}
 	return ((char *)s);
 }
