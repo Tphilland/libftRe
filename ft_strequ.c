aefcc581a441cb98f9a40d1b71c99f6bee6aa723
Tphilland <tphillan@student.wethinkcode.co.za>
2019-08-19 02:37:13 +0200

    Lexographical comparison between s1 and s2. If the strings are equal the return is 0, otherwise 0

---
+++ ft_strequ.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+int	ft_strequ(char const *s1, char const *s2)
+{
+	while (*s1 == *s2 && *s1++ != '\0' && *s2++ != '\0')
+	{
+		s1++;
+		s2++;
+	}
+	if (!(*s1 == *s2))
+		return (0);
+	else
+		return (1);
+}
