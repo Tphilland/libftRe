8649c066b84affc807288eb0136d3e48beb02fe7
Tphilland <tphillan@student.wethinkcode.co.za>
2019-09-06 03:29:40 +0200

    Allocates (with malloc(3)) and returns a “fresh” string ending with ’\0’, result of the concatenation of s1 and s2. If the allocation fails the function returns NULL.

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -14,6 +14,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	j = ft_strlen(s2);
 	i = i + j;
 	j = 0;
+	if (!(*s1 || *s2))
+		return (NULL);
 	t = (char *)malloc(i * sizeof(char) + 1);
 	i = 0;
 	while (s1[j] != '\0')
